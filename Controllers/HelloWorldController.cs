01885bce6189ca674fa700543f45c2dcf935f024
Yen-Ju-Chen <61488935+Yen-Ju-Chen@users.noreply.github.com>
2021-04-09 22:01:03 +0800

    Add files via upload

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+//命名為 HelloWorldController就可使用以下方法設定url
+//controller, action, id
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //呼叫view層
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
+
